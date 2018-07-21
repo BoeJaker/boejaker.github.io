a9a8b777045b1db172edab6943d7081f298d9f67
Boe Jaker <j.baker.cwp@gmail.com>
2018-07-21 23:57:06 +0100

    Add files via upload

---
+++ scripts/Draw.js
@@ -0,0 +1,31 @@
+TweenLite.defaultEase = Linear.easeNone;
+
+//show the square only once js has run
+//visibility set to hidden in css panel
+TweenLite.set(".square", {visibility:"visible"});
+
+
+
+var tl = new TimelineLite(), tl2= new TimelineLite(), tl3 = new TimelineLite(), tl4 = new TimelineLite();
+
+tl.fromTo(".l2", 4, {width:0}, {width:$('.table').outerWidth()});
+tl2.fromTo(".l1", 4, {height:0}, {height:$(".table").outerHeight()});
+tl3.fromTo(".l3", 4, {height:0}, {height:$(".table").outerHeight()});
+tl4.fromTo(".l4", 4, {width:0}, {width:$('.table').outerWidth()});
+
+tl.timeScale(4); //play faster
+tl2.timeScale(4); //play faster
+tl3.timeScale(4); //play faster
+tl4.timeScale(4); //play faster
+
+$('.square').css('height', $('.table').outerHeight());
+
+$( window ).resize(function() {$(".l1").css("height", $(".table").outerHeight());});
+$( window ).resize(function() {$('.l2').css('width', $('.table').outerWidth());});
+$( window ).resize(function() {$('.l3').css('height', $('.table').outerHeight());});
+$( window ).resize(function() {$('.l4').css('width', $('.table').outerWidth());});
+$( window ).resize(function() {$('.square').css('height', $('.table').outerHeight());}); 
+$( window ).resize(function() {$('page').css('min-height', $('.table').outerHeight()+100);}); 
+
+$('.trans--grow').addClass('grow');
+
