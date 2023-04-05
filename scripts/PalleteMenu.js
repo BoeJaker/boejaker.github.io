d445fdd378ceacebc652f2c375e4b29d6dc74cc0
boejaker <j.baker.cwp@gmail.com>
2023-04-05 05:44:17 +0100

    Changed the banner, pallette nav and scroll animations

--- scripts/PalleteMenu.js
+++ scripts/PalleteMenu.js
@@ -58,6 +58,20 @@ $(window).scroll(function(){
 	}, 250));
 		};
 });
+$('.content').on('touchmove', function() {
+  if ($('.page').outerHeight() <= $(window).height()) {
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+      var individualDivHeight = $('.page').height();
+      var _cur_top = $(window).scrollTop();
+      var totalHeight = $('.content').height();
+      var posToScroll = Math.round(_cur_top / individualDivHeight) * individualDivHeight;
+      // Use native browser scrolling instead of jQuery's animate()
+      $('html, body').css('scroll-behavior', 'smooth');
+      window.scrollTo({ top: posToScroll, behavior: 'smooth' });
+    }, 250));
+  }
+});
 
 // document.getElementById('welcome_pallete').addEventListener('click', function() {
 //   location.href = '#welcome'
